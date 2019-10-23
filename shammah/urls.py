2a4821cb72b31fed2fb5c3a384139301d6b80192
sun0511 <sun0511777@gmail.com>
2019-10-23 16:42:28 +0900

    final

--- shammah/urls.py
+++ shammah/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import re_path, path
 from graphene_django.views import GraphQLView
+from shammah.schema import schema
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    re_path(r'^graphql$', GraphQLView.as_view(graphiql=True)),
+    path('admin/', admin.site.urls),
+    re_path(r'^graphql$', GraphQLView.as_view(graphiql=True, schema=schema))
+
 ]
